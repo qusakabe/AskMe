bf18d900707817e8c8ad67b2cbdf82b59b0f424a
Your Name <you@example.com>
2025-04-08 15:52:52 +0300

    add: dotenv

--- ask_pavlovskii/settings.py
+++ ask_pavlovskii/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,11 +79,14 @@ WSGI_APPLICATION = 'ask_pavlovskii.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': f'{os.getenv("DB_NAME")}',
+        'USER': f'{os.getenv("DB_USER")}',
+        'PASSWORD': f'{os.getenv("DB_PASSWORD")}',
+        'HOST': f'{os.getenv("DB_HOST")}',
+        'PORT': f'{os.getenv("DB_PORT")}',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
