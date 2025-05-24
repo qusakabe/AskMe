471d93802fb6ed70103afb5198a0d92f5d5d2dde
Your Name <you@example.com>
2025-05-24 16:25:02 +0300

    add: form views

--- app/views.py
+++ app/views.py
@@ -1,8 +1,15 @@
+from audioop import reverse
+
+from django.contrib import auth
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.paginator import PageNotAnInteger, EmptyPage
 from app.models import Question, TagManager, Tag, Answer
 from django.http import HttpResponseNotFound
+from .forms import signupForm, loginForm, settingsForm, questionForm, answerForm
+from django.contrib.auth import logout
+from django.urls import reverse
+from math import ceil
 
 
 def paginate(objects_list, request, per_page=10):
@@ -20,6 +27,14 @@ def paginate(objects_list, request, per_page=10):
         page = paginator.page(paginator.num_pages)
 
     return page
+
+def get_page_number_for_answer(answers_queryset, target_answer, per_page):
+    answer_ids = list(answers_queryset.values_list('id', flat=True))
+    try:
+        index = answer_ids.index(target_answer.id)
+        return ceil((index + 1) / per_page)
+    except ValueError:
+        return 1
 
 def index(request):
     questions = Question.objects.get_new()
@@ -39,7 +54,17 @@ def question(request,id):
     page = paginate(answers,request)
     tags = Tag.objects.get_top()
 
-    return render(request, 'question.html', context={'page': page, 'answers': answers, 'question': question, 'tags': tags})
+    form = answerForm()
+    if request.method == 'POST':
+        form = answerForm(request.POST,user=request.user, question=question)
+        if form.is_valid():
+            answer = form.save()
+            all_answers = Answer.objects.get_by_question(question=question)
+            page_number = get_page_number_for_answer(all_answers, answer, per_page=10)
+            url = f"{reverse('question', kwargs={'id': question.id})}?page={page_number}#answer-{answer.id}"
+            return redirect(url)
+    return render(request, 'question.html', context={'page': page, 'answers': answers, 'question': question, 'tags': tags, 'form': form})
+
 
 def tag(request,id):
     try:
@@ -56,18 +81,54 @@ def tag(request,id):
 
 def ask(request):
     tags = Tag.objects.get_top()
-    return render(request, 'ask.html',context={'tags': tags})
+    form = questionForm()
+    if request.method == 'POST':
+        form = questionForm(request.POST, user=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('question', kwargs={'id': form.instance.id}))
+    return render(request, 'ask.html',context={'tags': tags, 'form': form})
 
 def login(request):
     tags = Tag.objects.get_top()
-    return render(request, 'login.html',context={'tags': tags})
+    form = loginForm()
+    if request.method == 'POST':
+        form = loginForm(request.POST)
+        if form.is_valid():
+            user = auth.authenticate(request, **form.cleaned_data)
+            if user:
+                auth.login(request,user)
+                continue_url = request.GET.get('continue', '/')
+                return redirect(continue_url)
+            form.add_error('password','Error password or login!')
+    return render(request, 'login.html',context={'tags': tags, 'form': form})
 
 def signup(request):
     tags = Tag.objects.get_top()
-    return render(request, 'signup.html',context={'tags': tags})
+    form = signupForm()
+    if request.method == 'POST':
+        form = signupForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            user = auth.authenticate(request, **form.cleaned_data)
+            if user:
+                auth.login(request, user)
+    return render(request, 'signup.html', context={'tags':tags,'form': form})
 
 def settings(request):
-    return render(request,'settings.html')
+    tags = Tag.objects.get_top()
+    form = settingsForm(
+            user=request.user,
+            initial={
+                'username': request.user.username,
+                'email': request.user.email,
+                'first_name': request.user.first_name,
+            })
+    if request.method == 'POST':
+        form = settingsForm(request.POST, request.FILES, user=request.user)
+        if form.is_valid():
+            form.save()
+    return render(request,'settings.html', context={'tags':tags,'form': form})
 
 def hot(request):
     questions = Question.objects.get_top()
@@ -75,3 +136,8 @@ def hot(request):
     tags = Tag.objects.get_top()
 
     return render(request, 'hot.html', context={'page': page, 'questions': questions, 'tags': tags})
+
+def logout_view(request):
+    logout(request)
+    continue_url = request.GET.get('continue', '/')
+    return redirect(continue_url)
