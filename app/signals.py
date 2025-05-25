29db53c828f14c63d75c3b6425026478779112d1
Your Name <you@example.com>
2025-05-25 18:56:12 +0300

    add: update answers count

--- app/signals.py
+++ app/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Answer
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -11,3 +11,8 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+@receiver(post_save, sender=Answer)
+def update_question_answer_count_on_save(sender, instance, created, **kwargs):
+    if created:
+        instance.question.update_answer_count()
